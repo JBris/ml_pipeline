cd49b9656889b7c3a1d21d86b506da7b2c8b025f
JBris <james.a.bristow.contact@gmail.com>
2022-02-25 12:17:11 +1300

    Adding MLFlow-Prometheus integration

--- mlflow_local/mlflow_server.sh
+++ mlflow_local/mlflow_server.sh
@@ -9,4 +9,4 @@
 echo "Host: ${MLFLOW_LOCAL_HOST}" 
 echo "Port: ${MLFLOW_LOCAL_PORT}"
 
-mlflow server --serve-artifacts --host "${MLFLOW_LOCAL_HOST}" --port "${MLFLOW_LOCAL_PORT}" --default-artifact-root file:${PROJECT_DIR}/mlflow_local/mlruns --backend-store-uri sqlite:///mlflow.db 
+mlflow server --serve-artifacts --host "${MLFLOW_LOCAL_HOST}" --port "${MLFLOW_LOCAL_PORT}" --default-artifact-root file:${PROJECT_DIR}/mlflow_local/mlruns --backend-store-uri "${MLFLOW_BACKEND_STORE_URI}" ${MLFLOW_EXPOSE_PROMETHEUS}
